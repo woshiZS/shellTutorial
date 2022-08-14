8c92cba960895e19dbee3879dfeb326215d41ec1
Jason Heywood <jasonzhansen@foxmail.com>
2022-08-14 15:51:20 +0800

    finish str and array part of sh

---
+++ array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+my_arr=(smt, "std", 345, 4567er)
+my_arr[0]=smthing
+:<<7
+  echo ${my_arr[0]}
+  echo ${my_arr[1]}
+  echo ${my_arr[2]}
+  echo ${my_arr[3]}
+  my_arr[5]=sorry
+7
+echo ${my_arr[4]}
+echo ${my_arr[@]}
+echo ${#my_arr[@]}
+echo ${#my_arr[*]}
+echo ${#my_arr[0]}
