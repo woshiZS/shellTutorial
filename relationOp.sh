f188c2b8a4551101014532ccda8cbaae3f39e0c9
Jason Heywood <jasonzhansen@foxmail.com>
2022-08-15 13:09:53 +0800

    add operator notes ans several basic cmd

--- relationOp.sh
+++ relationOp.sh
@@ -2,6 +2,44 @@
 a=10
 b=20
 
-if [ $a -eq $b]
+if [ $a -eq $b ]
 then
-  echo "
+  echo "$a -eq $b : a equals to b"
+else
+  echo "$a -eq $b : a is not equal to b"
+fi
+
+if [ $a -ne $b ]
+then
+  echo "$a -ne $b : a is not equal to b"
+else
+  echo "$a -ne $b : a is equal to b"
+fi
+
+if [ $a -gt $b ]
+then
+  echo "$a -gt $b : a is greater than b"
+else
+  echo "$a -gt $b : a is less than or equal to b"
+fi
+
+if [ $a -lt $b ] 
+then
+  echo "$a -lt $b : a is less than b"
+else
+  echo "$a -lt $b : a is greater than or equal to b"
+fi
+
+if [ $a -ge $b ]
+then
+  echo "$a -ge $b : a is greater than or equal to b"
+else
+  echo "$a -ge $b : a is less than b"
+fi
+
+if [ $a -le $b ]
+then
+  echo "$a -le $b : a is less than or equal to b"
+else
+  echo "$a -le $b : a is greater than b"
+fi
---
+++ strOp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a="abc"
+b=abc
+
+if [ $a = $b ]
+then
+  echo "a equal to b"
+fi
